6464d1a8587ab0688a5a04244b9057af2b4c32ea
Luke Iannini <lukexi@me.com>
2017-11-04 01:34:45 -0700

    Tidying

--- main.c
+++ main.c
@@ -18,13 +18,45 @@
 #include "libavformat/avformat.h"
 #include "libavutil/imgutils.h"
 
+void RenderFrame(AVCodecContext* CodecContext, AVFrame* Frame,
+    SDL_Window* Window, GLuint QuadProgram, GLuint Quad,
+    GLuint YTex, GLuint UTex, GLuint VTex)
+{
+    UpdateTexture(YTex, CodecContext->width, CodecContext->height, GL_RED, Frame->data[0], Frame->linesize[0]); // Y pixels
+    UpdateTexture(UTex, CodecContext->width*0.5, CodecContext->height*0.5, GL_RED, Frame->data[1], Frame->linesize[1]); // U pixels
+    UpdateTexture(VTex, CodecContext->width*0.5, CodecContext->height*0.5, GL_RED, Frame->data[2], Frame->linesize[2]); // V pixels
+
+    glClearColor(0, 0.1, 0.1, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUniform1i(glGetUniformLocation(QuadProgram, "uTexY"), 0);
+    glUniform1i(glGetUniformLocation(QuadProgram, "uTexU"), 1);
+    glUniform1i(glGetUniformLocation(QuadProgram, "uTexV"), 2);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, YTex);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, UTex);
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, VTex);
+
+    glBindVertexArray(Quad);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+    SDL_GL_SwapWindow(Window);
+    SDL_Event Event;
+    while (SDL_PollEvent(&Event)) {
+        if (Event.type == SDL_QUIT) exit(0);
+    }
+}
+
 static int VideoDecodeExample(const char *InputFilename, SDL_Window* Window)
 {
-    AVCodec *Codec = NULL;
-    AVCodecContext *CodecContext= NULL;
-    AVCodecParameters *CodecParams = NULL;
-    AVFrame *Frame = NULL;
-    uint8_t *ByteBuffer = NULL;
+    AVCodec* Codec = NULL;
+    AVCodecContext* CodecContext= NULL;
+    AVCodecParameters* CodecParams = NULL;
+    AVFrame* Frame = NULL;
+    uint8_t* ByteBuffer = NULL;
     AVPacket Packet;
     AVFormatContext *FormatContext = NULL;
     int NumberOfWrittenBytes;
@@ -85,13 +117,6 @@ static int VideoDecodeExample(const char *InputFilename, SDL_Window* Window)
         return AVERROR(ENOMEM);
     }
 
-    ByteBufferSize = av_image_get_buffer_size(CodecContext->pix_fmt, CodecContext->width, CodecContext->height, 16);
-    ByteBuffer = av_malloc(ByteBufferSize);
-    if (!ByteBuffer) {
-        av_log(NULL, AV_LOG_ERROR, "Can't allocate buffer\n");
-        return AVERROR(ENOMEM);
-    }
-
     GLuint QuadProgram = CreateVertFragProgramFromPath(
         "quad.vert",
         "quad.frag");
@@ -109,7 +134,8 @@ static int VideoDecodeExample(const char *InputFilename, SDL_Window* Window)
     };
     GLuint Quad = CreateQuad(Verts);
 
-    printf("#tb %d: %d/%d\n", VideoStream, FormatContext->streams[VideoStream]->time_base.num, FormatContext->streams[VideoStream]->time_base.den);
+    AVRational Timebase = FormatContext->streams[VideoStream]->time_base;
+    printf("Timebase for video stream %d: %d/%d\n", VideoStream, Timebase.num, Timebase.den);
     FrameNumber = 0;
     av_init_packet(&Packet);
     do {
@@ -126,6 +152,7 @@ static int VideoDecodeExample(const char *InputFilename, SDL_Window* Window)
             GotFrame = 0;
 
             if (Packet.pts == AV_NOPTS_VALUE) {
+                printf("Setting frame pts\n");
                 Packet.pts = Packet.dts = FrameNumber;
             }
 
@@ -136,51 +163,14 @@ static int VideoDecodeExample(const char *InputFilename, SDL_Window* Window)
             }
 
             if (GotFrame) {
-                NumberOfWrittenBytes = av_image_copy_to_buffer(
-                    ByteBuffer, ByteBufferSize,
-                    (const uint8_t* const *)Frame->data,
-                    (const int*)Frame->linesize,
-                    CodecContext->pix_fmt,
-                    CodecContext->width,
-                    CodecContext->height,
-                    1);
-                if (NumberOfWrittenBytes < 0) {
-                    av_log(NULL, AV_LOG_ERROR, "Can't copy image to buffer\n");
-                    return NumberOfWrittenBytes;
-                }
-                printf("%d, %10"PRId64", %10"PRId64", %8"PRId64", %8d, 0x%08lx\n", VideoStream,
-                        Frame->pts, Frame->pkt_dts, Frame->pkt_duration,
-                        NumberOfWrittenBytes, av_adler32_update(0, (const uint8_t*)ByteBuffer, NumberOfWrittenBytes));
-                printf("%s\n", av_get_pix_fmt_name(CodecContext->pix_fmt));
+                printf("%10"PRId64", %10"PRId64", %8"PRId64"\n",
+                        Frame->pts, Frame->pkt_dts, Frame->pkt_duration);
+                printf("Uploading %s frame of %i x %i\n",
+                    av_get_pix_fmt_name(CodecContext->pix_fmt),
+                    CodecContext->width, CodecContext->height);
 
-                printf("Uploading %i %i %i bytes\n", ByteBufferSize, CodecContext->width, CodecContext->height);
-                UpdateTexture(YTex, CodecContext->width, CodecContext->height, GL_RED, Frame->data[0], Frame->linesize[0]); // Y pixels
-                UpdateTexture(UTex, CodecContext->width*0.5, CodecContext->height*0.5, GL_RED, Frame->data[1], Frame->linesize[1]); // U pixels
-                UpdateTexture(VTex, CodecContext->width*0.5, CodecContext->height*0.5, GL_RED, Frame->data[2], Frame->linesize[2]); // V pixels
+                RenderFrame(CodecContext, Frame, Window, QuadProgram, Quad, YTex, UTex, VTex);
 
-                glClearColor(0, 0.1, 0.1, 1);
-                glClear(GL_COLOR_BUFFER_BIT);
-
-                glUniform1i(glGetUniformLocation(QuadProgram, "uTexY"), 0);
-                glUniform1i(glGetUniformLocation(QuadProgram, "uTexU"), 1);
-                glUniform1i(glGetUniformLocation(QuadProgram, "uTexV"), 2);
-
-                glActiveTexture(GL_TEXTURE0);
-                glBindTexture(GL_TEXTURE_2D, YTex);
-                glActiveTexture(GL_TEXTURE1);
-                glBindTexture(GL_TEXTURE_2D, UTex);
-                glActiveTexture(GL_TEXTURE2);
-                glBindTexture(GL_TEXTURE_2D, VTex);
-                printf("i: %i\n", i);
-
-                glBindVertexArray(Quad);
-                glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-                SDL_GL_SwapWindow(Window);
-                SDL_Event Event;
-                while (SDL_PollEvent(&Event)) {
-                    if (Event.type == SDL_QUIT) exit(0);
-                }
                 FrameNumber++;
             }
             av_packet_unref(&Packet);
@@ -194,7 +184,6 @@ static int VideoDecodeExample(const char *InputFilename, SDL_Window* Window)
     avcodec_close(CodecContext);
     avformat_close_input(&FormatContext);
     avcodec_free_context(&CodecContext);
-    av_freep(&ByteBuffer);
     return 0;
 }
 
