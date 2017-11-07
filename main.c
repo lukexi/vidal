7c1807401b3baeb446c51bbe9de8542076c17fa4
Luke Iannini <lukexi@me.com>
2017-11-07 12:37:46 -0800

    Separate video opening from decoding

--- main.c
+++ main.c
@@ -50,82 +50,102 @@ void RenderFrame(AVCodecContext* CodecContext, AVFrame* Frame,
     }
 }
 
-static int VideoDecodeExample(const char *InputFilename, SDL_Window* Window)
-{
-    AVCodec* Codec = NULL;
-    AVCodecContext* CodecContext= NULL;
-    AVCodecParameters* CodecParams = NULL;
-    AVFrame* Frame = NULL;
-    uint8_t* ByteBuffer = NULL;
-    AVPacket Packet;
-    AVFormatContext *FormatContext = NULL;
-    int NumberOfWrittenBytes;
+typedef struct {
+    AVCodec*         Codec;
+    AVFrame*         Frame;
+    AVPacket         Packet;
+    AVCodecParameters* CodecParams;
+    AVCodecContext*  CodecContext;
+    AVFormatContext* FormatContext;
     int VideoStream;
-    int GotFrame = 0;
-    int ByteBufferSize;
-    int FrameNumber = 0;
-    int Result;
-    int EndOfStream = 0;
-    int EndOfFile = 0;
+    int AudioStream;
+} video;
 
-    Result = avformat_open_input(&FormatContext, InputFilename, NULL, NULL);
+video* OpenVideo(const char* InputFilename) {
+    video* Video = calloc(1, sizeof(video));
+
+    int Result;
+
+    Result = avformat_open_input(&Video->FormatContext, InputFilename, NULL, NULL);
     if (Result < 0) {
         av_log(NULL, AV_LOG_ERROR, "Can't open file\n");
-        return Result;
+        free(Video);
+        return NULL;
     }
 
-    Result = avformat_find_stream_info(FormatContext, NULL);
+    Result = avformat_find_stream_info(Video->FormatContext, NULL);
     if (Result < 0) {
         av_log(NULL, AV_LOG_ERROR, "Can't get stream info\n");
-        return Result;
+        free(Video);
+        return NULL;
     }
 
-    VideoStream = av_find_best_stream(FormatContext, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
-    if (VideoStream < 0) {
+    Video->VideoStream = av_find_best_stream(Video->FormatContext, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
+    if (Video->VideoStream < 0) {
         av_log(NULL, AV_LOG_ERROR, "Can't find video stream in input file\n");
-        return -1;
+        free(Video);
+        return NULL;
     }
+    Video->AudioStream = av_find_best_stream(Video->FormatContext, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
 
-    CodecParams = FormatContext->streams[VideoStream]->codecpar;
+    Video->CodecParams = Video->FormatContext->streams[Video->VideoStream]->codecpar;
 
-    Codec = avcodec_find_decoder(CodecParams->codec_id);
-    if (!Codec) {
+    Video->Codec = avcodec_find_decoder(Video->CodecParams->codec_id);
+    if (!Video->Codec) {
         av_log(NULL, AV_LOG_ERROR, "Can't find decoder\n");
-        return -1;
+        free(Video);
+        return NULL;
     }
 
-    CodecContext = avcodec_alloc_context3(Codec);
-    if (!CodecContext) {
+    Video->CodecContext = avcodec_alloc_context3(Video->Codec);
+    if (!Video->CodecContext) {
         av_log(NULL, AV_LOG_ERROR, "Can't allocate decoder context\n");
-        return AVERROR(ENOMEM);
+        // AVERROR(ENOMEM);
+        free(Video);
+        return NULL;
     }
 
-    Result = avcodec_parameters_to_context(CodecContext, CodecParams);
+    Result = avcodec_parameters_to_context(Video->CodecContext, Video->CodecParams);
     if (Result) {
         av_log(NULL, AV_LOG_ERROR, "Can't copy decoder context\n");
-        return Result;
+        free(Video);
+        return NULL;
     }
 
-    Result = avcodec_open2(CodecContext, Codec, NULL);
+    Result = avcodec_open2(Video->CodecContext, Video->Codec, NULL);
     if (Result < 0) {
-        av_log(CodecContext, AV_LOG_ERROR, "Can't open decoder\n");
-        return Result;
+        av_log(Video->CodecContext, AV_LOG_ERROR, "Can't open decoder\n");
+        free(Video);
+        return NULL;
     }
 
-    Frame = av_frame_alloc();
-    if (!Frame) {
+    Video->Frame = av_frame_alloc();
+    if (!Video->Frame) {
         av_log(NULL, AV_LOG_ERROR, "Can't allocate frame\n");
-        return AVERROR(ENOMEM);
+        // return AVERROR(ENOMEM);
+        free(Video);
+        return NULL;
     }
+    return Video;
+}
+
+static int VideoDecodeExample(video* Video, SDL_Window* Window) {
+
+    int GotFrame = 0;
+    int FrameNumber = 0;
+    int Result;
+
+    int EndOfStream = 0;
+    int EndOfFile = 0;
 
     GLuint QuadProgram = CreateVertFragProgramFromPath(
         "quad.vert",
         "quad.frag");
     glUseProgram(QuadProgram);
 
-    GLuint YTex = CreateTexture(CodecContext->width,     CodecContext->height,     1);
-    GLuint UTex = CreateTexture(CodecContext->width*0.5, CodecContext->height*0.5, 1);
-    GLuint VTex = CreateTexture(CodecContext->width*0.5, CodecContext->height*0.5, 1);
+    GLuint YTex = CreateTexture(Video->CodecContext->width,     Video->CodecContext->height,     1);
+    GLuint UTex = CreateTexture(Video->CodecContext->width*0.5, Video->CodecContext->height*0.5, 1);
+    GLuint VTex = CreateTexture(Video->CodecContext->width*0.5, Video->CodecContext->height*0.5, 1);
 
     float Verts[8] = {
         -1, -1, // Left Top
@@ -135,42 +155,38 @@ static int VideoDecodeExample(const char *InputFilename, SDL_Window* Window)
     };
     GLuint Quad = CreateQuad(Verts);
 
-    AVRational Timebase = FormatContext->streams[VideoStream]->time_base;
-    printf("Timebase for video stream %d: %d/%d\n", VideoStream, Timebase.num, Timebase.den);
+    AVRational Timebase = Video->FormatContext->streams[Video->VideoStream]->time_base;
+    printf("Timebase for video stream %d: %d/%d\n", Video->VideoStream, Timebase.num, Timebase.den);
     FrameNumber = 0;
-    av_init_packet(&Packet);
+    av_init_packet(&Video->Packet);
     do {
         if (!EndOfStream) {
-            if (av_read_frame(FormatContext, &Packet) < 0) {
+            if (av_read_frame(Video->FormatContext, &Video->Packet) < 0) {
                 EndOfStream = 1;
             }
         }
         if (EndOfStream) {
-            Packet.data = NULL;
-            Packet.size = 0;
+            Video->Packet.data = NULL;
+            Video->Packet.size = 0;
         }
-        if (Packet.stream_index == VideoStream || EndOfStream) {
+        if (Video->Packet.stream_index == Video->AudioStream || EndOfStream) {
+
+        }
+        if (Video->Packet.stream_index == Video->VideoStream || EndOfStream) {
             GotFrame = 0;
 
-            if (Packet.pts == AV_NOPTS_VALUE) {
+            if (Video->Packet.pts == AV_NOPTS_VALUE) {
                 printf("Setting frame pts\n");
-                Packet.pts = Packet.dts = FrameNumber;
+                Video->Packet.pts = Video->Packet.dts = FrameNumber;
             }
 
-            /*
-            Result = avcodec_decode_video2(CodecContext, Frame, &GotFrame, &Packet);
-            if (Result < 0) {
-                av_log(NULL, AV_LOG_ERROR, "Error decoding frame\n");
-                return Result;
-            }
-            */
-            Result = avcodec_send_packet(CodecContext, &Packet);
+            Result = avcodec_send_packet(Video->CodecContext, &Video->Packet);
             if (Result != 0) {
                 av_log(NULL, AV_LOG_ERROR, "Error sending packet\n");
                 return Result;
             }
 
-            Result = avcodec_receive_frame(CodecContext, Frame);
+            Result = avcodec_receive_frame(Video->CodecContext, Video->Frame);
             if (Result != 0 && Result != AVERROR_EOF) {
                 av_log(NULL, AV_LOG_ERROR, "Error receiving frame\n");
                 return Result;
@@ -182,29 +198,32 @@ static int VideoDecodeExample(const char *InputFilename, SDL_Window* Window)
 
             if (GotFrame) {
                 printf("%10"PRId64", %10"PRId64", %8"PRId64"\n",
-                    Frame->pts, Frame->pkt_dts, Frame->pkt_duration);
+                    Video->Frame->pts, Video->Frame->pkt_dts, Video->Frame->pkt_duration);
                 printf("Uploading %s frame of %i x %i\n",
-                    av_get_pix_fmt_name(CodecContext->pix_fmt),
-                    CodecContext->width, CodecContext->height);
+                    av_get_pix_fmt_name(Video->CodecContext->pix_fmt),
+                    Video->CodecContext->width, Video->CodecContext->height);
 
-                RenderFrame(CodecContext, Frame, Window, QuadProgram, Quad, YTex, UTex, VTex);
+                RenderFrame(Video->CodecContext, Video->Frame, Window, QuadProgram, Quad, YTex, UTex, VTex);
 
                 FrameNumber++;
             }
 
-
-            av_packet_unref(&Packet);
-            av_init_packet(&Packet);
+            av_packet_unref(&Video->Packet);
+            av_init_packet(&Video->Packet);
         }
 
     } while (!EndOfStream || GotFrame);
 
-    av_packet_unref(&Packet);
-    av_frame_free(&Frame);
-    avcodec_close(CodecContext);
-    avformat_close_input(&FormatContext);
-    avcodec_free_context(&CodecContext);
+
     return 0;
+}
+
+void FreeVideo(video* Video) {
+    av_packet_unref(&Video->Packet);
+    av_frame_free(&Video->Frame);
+    avcodec_close(Video->CodecContext);
+    avformat_close_input(&Video->FormatContext);
+    avcodec_free_context(&Video->CodecContext);
 }
 
 #define NUM_QUADS 1
@@ -226,7 +245,8 @@ int main(int argc, char const *argv[]) {
     SDL_GL_MakeCurrent(Window, GLContext);
     InitGLEW();
 
-    if (VideoDecodeExample("pinball.mov", Window) != 0) {
+    video* Video = OpenVideo("pinball.mov");
+    if (VideoDecodeExample(Video, Window) != 0) {
         return 1;
     }
 
